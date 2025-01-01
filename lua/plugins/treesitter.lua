4161769cd035d282eb9f5bba1c6940618a916514
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-01-02 01:55:35 +0600

    2_Jan_25|2:55AM

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -21,7 +21,7 @@ return {
             },
             sync_install = false,
             auto_install = true,
-            highlight = { enable = true },
+            highlight = { enable = false },
             indent = { enable = true },
             incremental_selection = {
                 enable = true,
